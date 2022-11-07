fcffb8cb22e729d76d13668aee24bc4ebaf04d32
julio neto <julio.neto@icloud.com>
2022-11-07 08:24:27 -0300

    clean code

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ require_once 'app/controller/UsuarioController.php';
 
 use App\Controller\UsuarioController;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if (!$_GET['url']) {
     header("Location: home");
     die;
@@ -18,26 +14,21 @@ $route = (explode('/', $_GET['url']))[0];
 switch ($route) {
     case 'home':
         require_once 'resources/view/index.php';
-        die;
-
         break;
     case 'endpoint':
         if (empty((explode('/', $_GET['url']))[1]) && empty((explode('/', $_GET['url']))[2])) {
             return header("Location: home?failed=endpoint_params");
-            die;
         }
         $email = (explode('/', $_GET['url']))[1];
         $senha = (explode('/', $_GET['url']))[2];
         $_controlllerUsuario = new UsuarioController();
         $data = $_controlllerUsuario->getProducts($email, $senha);
         echo json_encode($data);
-        die;
         break;
     case 'list-usuario':
         $_controlllerUsuario = new UsuarioController();
         $data = $_controlllerUsuario->index();
         echo json_encode($data);
-        die;
         break;
     case 'update-usuario':
 
@@ -49,13 +40,11 @@ switch ($route) {
             die;
         }
         echo 'falha';
-        die;
         break;
     case 'delete-usuario':
         $data = json_decode(file_get_contents('php://input'), true);
         $_controlllerUsuario = new UsuarioController();
         $data = $_controlllerUsuario->delete($data['id']);
-        die;
         break;
     case 'create-usuario':
         $data = json_decode(file_get_contents('php://input'), true);
@@ -66,33 +55,6 @@ switch ($route) {
         break;
     default:
         break;
+
+    die;
 }
- // require_once 'app/model/produto/Mapper.php';
-// require_once 'app/model/usuario/Mapper.php';
-
-// use Produto\Mapper as PMapper;
-// use Usuario\Mapper as UMapper;
-
-// $usuario = new UMapper();
-// $usuario->setId(1);
-// $usuario->setEmail('julio@admin.com.br');
-// $usuario->setSenha('senha');
-// $usuario->update();
-
-// $produto = new PMapper();
-// $produto->setNome('teste up');
-// $produto->setPreco(2700);
-// $produto->insert();
-
-// echo '<br />';
-// echo '<br />';
-// echo '<br />';
-// echo '<br />';
-// var_dump($produto::findById(2, 'produto'));
-// echo '<br />';
-// var_dump( $produto::findByName('telefone', 'produto'));
-// echo '<br />';
-// echo '<br />';
-// print_r( $produto::findById('1', 'usuario'));
-// echo '<br />';
-// die; -->
